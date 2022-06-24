fc18306182a89fff78650dc2fde44b0aaea63ba3
Nursultan Berdiev <nursultanberdiev1994@gmail.com>
2022-06-24 19:56:57 +0600

    final commit

--- courses/settings.py
+++ courses/settings.py
@@ -141,8 +141,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/static/'
-MEDIA_URL = '/static/media/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
